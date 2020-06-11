091c725db1c53c5681b48609362a54376e21fd59
sk8shape <sk8shape@users.noreply.github.com>
2020-06-11 17:48:30 +0200

    moved functions into other files

---
+++ random_network.py
@@ -0,0 +1,74 @@
+import torch.nn as nn
+from torch.autograd import Function
+
+class ReverseLayerF(Function):
+    # Forwards identity
+    # Sends backward reversed gradients
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
+
+def grad_reverse(x, alpha):
+    return ReverseLayerF.apply(x, alpha)
+
+class RandomNetworkWithReverseGrad(nn.Module):
+    def __init__(self, **kwargs):
+        super(RandomNetworkWithReverseGrad, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 1000),
+        )
+        self.dann_classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 1000),
+        )
+
+    def forward(self, x, alpha=None):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        # If we pass alpha, we can assume we are training the discriminator
+        if alpha is not None:
+            # gradient reversal layer (backward gradients will be reversed)
+            x = grad_reverse(x, alpha)
+            discriminator_output = self.dann_classifier(x)
+            return discriminator_output
+        # If we don't pass alpha, we assume we are training with supervision
+        else:
+            #train the classifier with supervision
+            class_outputs = self.classifier(x)
+            return class_outputs
---
+++ train_functions.py
@@ -0,0 +1,127 @@
+def train_model(net, dataloader, criterion, scheduler, optimizer, num_epochs = NUM_EPOCHS):
+    DEVICE = "cuda"
+    since = time.time()
+    cudnn.benchmark
+    net = net.to(DEVICE)
+    current_step = 0
+    loss_array = []
+    step_array = []
+    for epoch in range(NUM_EPOCHS):
+        print('Starting epoch {}/{}, LR = {}'.format(epoch+1, NUM_EPOCHS,
+                                                     scheduler.get_last_lr()))
+
+
+        for images,labels in dataloader:
+            images = images.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            net.train()
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+
+
+            if current_step % LOG_FREQUENCY == 0:
+                print('Step {}, Loss {}'.format(current_step, loss.item()))
+                loss_array.append(loss)
+                step_array.append(current_step)
+
+            loss.backward()
+            optimizer.step()
+            current_step += 1
+
+        accuracy = test_model(net, art_dl, art_dataset)
+        accuracy_array.append(accuracy)
+        epochs_array.append(epoch)
+        scheduler.step()
+
+    plt.plot(step_array,loss_array)
+    plt.xlabel("Step number")
+    plt.ylabel("Loss")
+    plt.show()
+
+    plt.plot(epochs_array, accuracy_array)
+    plt.xlabel("Epochs number")
+    plt.ylabel("Accuracy")
+    plt.show()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    return net
+
+def test_model(net, dataloader, dataset):
+    DEVICE = 'cuda'
+    running_corrects = 0
+    for images, labels in tqdm(dataloader):
+        images = images.to(DEVICE)
+        labels = labels.to(DEVICE)
+
+        outputs = net(images)
+
+        _, preds = torch.max(outputs.data, 1)
+
+        running_corrects += torch.sum(preds == labels.data).data.item()
+
+    accuracy = running_corrects/float(len(dataset))
+    print('Test Accuracy: {}'.format(accuracy))
+    return accuracy
+
+##TRAIN ON PHOTO AND TEST ON ART WITH DANN ADAPTATION
+ALPHA_P = 0.01
+def train_model_with_dann(net, dataloader_src, dataloader_target, criterion, scheduler, optimizer, num_epochs = NUM_EPOCHS, batch_size = BATCH_SIZE, alpha = ALPHA_P):
+    DEVICE = "cuda"
+    since = time.time()
+    cudnn.benchmark
+    net = net.to(DEVICE)
+    current_step = 0
+
+    zeros = np.zeros(batch_size)
+    zero_target = torch.as_tensor(zeros).long()
+    zero_target = zero_target.to(DEVICE)
+    ones = np.ones(batch_size)
+    one_target = torch.as_tensor(ones).long()
+    one_target = one_target.to(DEVICE)
+
+    for epoch in range(NUM_EPOCHS):
+        print('Starting epoch {}/{}, LR = {}'.format(epoch+1, NUM_EPOCHS,
+                                                     scheduler.get_last_lr()))
+
+        for i in range(len(dataloader_src)):
+            images, labels = next(iter(dataloader_src))
+            images = images.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            net.train()
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+
+            outputs = net(images, alpha = ALPHA_P)
+            loss = criterion(outputs, zero_target)
+            loss.backward()
+
+            target_images, target_labels = next(iter(dataloader_target))
+
+            target_images = target_images.to(DEVICE)
+            target_labels = target_labels.to(DEVICE)
+
+            outputs = net(target_images, alpha=ALPHA_P)
+            loss = criterion(outputs, one_target)
+
+
+            if current_step % LOG_FREQUENCY == 0:
+                print('Step {}, Loss {}'.format(current_step, loss.item()))
+
+            loss.backward()
+            optimizer.step()
+            current_step += 1
+
+        scheduler.step()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print("alpha: " + str(ALPHA_P))
+
+    return net
